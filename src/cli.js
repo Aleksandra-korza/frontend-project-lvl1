01ec3b6fa89789decbc6789358ab3c075d41a3a1
Aleksandra-korza <Aleksandra.korza@mail.ru>
2021-05-26 13:55:46 +0200

    brainGames

--- src/cli.js
+++ src/cli.js
@@ -1,4 +1,12 @@
 import readlineSync from 'readline-sync';
 
 const name = readlineSync.question('Your answer: ');
-export default name;
+
+const brainGames = () => {
+  console.log('Welcome to the Brain Games!');
+  // eslint-disable-next-line no-use-before-define
+  console.log(`May I have your name? ${name}`);
+  // eslint-disable-next-line no-use-before-define
+  console.log(`Hello!${name}`);
+};
+export default brainGames;
