02ddb7dee01da46cb03d21c794b16ac6d9b33dd3
Aleksandra-korza <Aleksandra.korza@mail.ru>
2021-06-01 13:52:51 +0200

    поправка всех игр по пинципу добавления "getGameData"

--- src/brain-prime.js
+++ src/brain-prime.js
@@ -5,41 +5,36 @@ import getRandomNumb from './getRandomNumb.js';
 
 const brainPrime = () => {
   const getGameData = () => {
+    const questions = [];
     const createQuestions = (length = 3) => {
-      const questions = [];
       for (let i = 0; i < length; i += 1) {
         const question = getRandomNumb(0, 100);
         questions.push(question);
       }
-
-      const rules = 'Answer "yes" if given number is prime. Otherwise answer "no".';
-
-      const correctAnswers = [];
-      const answer = (x) => {
-        for (let i = 2; i < x - 1;) {
-          if ((x % i === 0) || (x === 0 || x === 1)) {
-            return false;
-          }
-          i += 1;
-        }
-        return true;
-      };
-
-      for (let i = 0; i < questions.length; i += 1) {
-        const x = questions[i];
-        if (answer(x) === false) {
-          correctAnswers.push('no');
-        } else {
-          correctAnswers.push('yes');
-        }
-      }
-      return {
-        questions,
-        correctAnswers,
-        rules,
-      };
     };
-    const { rules, questions, correctAnswers } = createQuestions();
+    createQuestions();
+
+    const rules = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+    const correctAnswers = [];
+    const answer = (x) => {
+      for (let i = 2; i < x - 1;) {
+        if ((x % i === 0) || (x === 0 || x === 1)) {
+          return false;
+        }
+        i += 1;
+      }
+      return true;
+    };
+
+    for (let i = 0; i < questions.length; i += 1) {
+      const x = questions[i];
+      if (answer(x) === false) {
+        correctAnswers.push('no');
+      } else {
+        correctAnswers.push('yes');
+      }
+    }
     return {
       questions,
       correctAnswers,
