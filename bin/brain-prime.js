f2af8647d52c669d40d386375d7e10456809aa52
Aleksandra-korza <Aleksandra.korza@mail.ru>
2021-06-08 11:56:17 +0200

    добавление папки games и перенос туда игр и исправление импорта

--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
 
-import game from '../src/brain-prime.js'; // скобки добавить к путю, такой синтаксис у импорта.
+import game from '../src/games/brain-prime.js'; // скобки добавить к путю, такой синтаксис у импорта.
 
 game();
