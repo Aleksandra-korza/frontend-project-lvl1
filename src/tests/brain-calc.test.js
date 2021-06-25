922e6ab8c6378034430564f74be277feda2d4bd7
Aleksandra-korza <Aleksandra.korza@mail.ru>
2021-06-25 15:58:43 +0200

    изменила имя запуска основн. игры на startGame

---
+++ src/tests/brain-calc.test.js
@@ -0,0 +1,7 @@
+import { strict as assert } from 'assert';
+import assert from 'power-assert';
+import gameCalc from '../games/brain-calc.js';
+
+assert.equal(gameCalc(1), 1);
+
+console.log('Все тесты пройдены!');
